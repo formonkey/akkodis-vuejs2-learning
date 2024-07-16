00a3d23e259032eaac3159e62f4592ab99a28c5e
Isaac Martín <misaacmartin@gmail.com>
2024-07-16 07:39:24 +0200

    First Commit

---
+++ app1/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Hello, World! - App 1</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App'
+}
+</script>
+
+<style>
+/* Estilos opcionales */
+</style>
---
+++ app2/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Hello, World! - App 2</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App'
+}
+</script>
+
+<style>
+/* Estilos opcionales */
+</style>
